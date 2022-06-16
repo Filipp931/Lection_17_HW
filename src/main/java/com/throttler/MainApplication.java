d32f57e4f737abbf078399e1bc044aedbafdb7e4
Fil <fil931@gmail.com>
2022-06-16 08:43:25 +0300

    boot

--- src/main/java/com/throttler/MainApplication.java
+++ src/main/java/com/throttler/MainApplication.java
@@ -2,10 +2,8 @@ package com.throttler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 
-@EnableAspectJAutoProxy
 @SpringBootApplication
 public class MainApplication {
 
